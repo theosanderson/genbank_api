a3c32c96d2ab0bb3becceb3b8e749ddf317e7190
Theo Sanderson <theo@theo.io>
2023-02-15 18:44:40 +0000

    update

---
+++ src/pages/api/genbank.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export default async (req, res) => {
+  const { accession } = req.query;
+  const url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=${accession}&rettype=gbwithparts`;
+  
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+    res.status(200).send(data);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+}
