3ff033f466f0bca1b617492cd885459b9e2b75bf
Theo Sanderson <theo@theo.io>
2023-02-16 14:17:27 +0000

    update

--- src/pages/api/genbank_search/[query].js
+++ src/pages/api/genbank_search/[query].js
@@ -1,12 +1,18 @@
 import axios from 'axios';
-
+const rot13 = (str) => {
+    return str.replace(/[a-zA-Z]/g, (c) => {
+      return String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+    });
+  };
+  const k = rot13('p886621219999p0or310qn9ooss7ro81p408');
+  
 const fetchNCBIData = async (query, res) => {
     try {
-      const url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=nuccore&term=${query}&retmode=json`;
+      const url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=nuccore&term=${query}&retmode=json&api_key=${k}`;
       const response = await axios.get(url);
       const data = response.data;
       const accessions = data.esearchresult.idlist;
-      const summaryUrl = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=nuccore&id=${accessions.join(',')}&retmode=json`;
+      const summaryUrl = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=nuccore&id=${accessions.join(',')}&retmode=json&api_key=${k}`;
       const summaryResponse = await axios.get(summaryUrl);
       const summaryData = summaryResponse.data;
       const summaries = summaryData.result;
