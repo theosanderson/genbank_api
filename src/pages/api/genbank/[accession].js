70ad0ebcbc6b5a02c35de4ed3f32aca7605833ec
Theo Sanderson <theo@theo.io>
2023-06-16 18:17:27 +0100

    Update [accession].js

--- src/pages/api/genbank/[accession].js
+++ src/pages/api/genbank/[accession].js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 export default async (req, res) => {
-  const { accession } = req.query;
+  const { accession, rettype } = req.query;
+
+  const type = rettype ? rettype : 'gbwithparts';
   
-  const url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=${accession}&rettype=gbwithparts&retmode=text`;
+  const url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=${accession}&rettype=${type}&retmode=text`;
   
   try {
     const response = await axios.get(url);
